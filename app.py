ef6f6962b6962b620e03ba342c7f01c25d03baf3
30209774 <saqib.mazhar@ucalgary.ca>
2025-08-10 01:32:55 -0600

    Created Data script and tested some query things

--- app.py
+++ app.py
@@ -1,1 +1,49 @@
-print("hello")
+import influxdb_client, os, time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.environ.get("INFLUXDB_TOKEN")
+org = "saqib"
+url = "http://localhost:8086"
+
+client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+
+bucket = "energy_raw"
+
+# write_api = client.write_api(write_options=SYNCHRONOUS)
+#
+# for value in range(5):
+#     point = (
+#         Point("measurement1")
+#         .tag("tagname1", "tagvalue1")
+#         .field("field1", value)
+#     )
+#     write_api.write(bucket=bucket, org="saqib", record=point)
+#     time.sleep(1)  # separate points by 1 second
+
+query_api = client.query_api()
+
+# query = """from(bucket: "energy_raw")
+#  |> range(start: -10m)
+#  |> filter(fn: (r) => r._measurement == "energy")
+#  |> mean()"""
+
+
+
+query = '''from(bucket: "energy_raw")
+  |> range(start: -30m, stop: now())
+  |> filter(fn: (r) => r._measurement == "energy" and r.device_id == "1002")
+  |> filter(fn: (r) => r._field == "temp_c")
+  |> aggregateWindow(every: 1m, fn: mean, createEmpty: false)
+  |> group(columns: [])
+  |> sort(columns: ["_time"])'''
+
+tables = query_api.query(query, org="saqib")
+
+# for table in tables:
+#   for record in table.records:
+#     print(record)
+
+print(tables[0].records)
---
+++ data_script.py
@@ -0,0 +1,42 @@
+import random
+
+import influxdb_client, os, time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.environ.get("INFLUXDB_TOKEN")
+org = "saqib"
+url = "http://localhost:8086"
+
+client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+
+bucket = "energy_raw"
+
+write_api = client.write_api(write_options=SYNCHRONOUS)
+
+count = 0
+while True:
+    time.sleep(1)
+    device_num = random.randint(1000,1006)
+    power_num = random.uniform(12.5, 17.8)
+    current_num  = random.uniform(18.5,32.5)
+    voltage_num = random.uniform(475.3,482.8)
+    temp_num = random.uniform(34.7,43.2)
+
+    point = (
+        Point("energy")
+        .tag("device_id", device_num)
+        .tag("site","SITE A")
+        .field("power_kw", power_num)
+        .field("current_a", current_num)
+        .field("voltage_v",voltage_num)
+        .field("temp_c",temp_num)
+    )
+    write_api.write(bucket=bucket, org="saqib", record=point)
+    count+=1
+    print(f"record: {count}\n{device_num} |{power_num} | {voltage_num} | {current_num} | {temp_num}")
+
+
+
